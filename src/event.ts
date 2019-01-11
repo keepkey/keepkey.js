7288ecc3fc011ba3143787e2fd6df5ec892247b8
keepkeyjon <jon@keepkey.com>
2019-01-11 09:25:44 -0700

    Address review feedback

--- src/event.ts
+++ src/event.ts
@@ -1,12 +1,6 @@
 import messageNameRegistry from './messageNameRegistry'
 import Messages from './kkProto/messages_pb'
 
-const { MessageType: {
-  MESSAGETYPE_PASSPHRASEACK,
-  MESSAGETYPE_ENTROPYACK,
-  MESSAGETYPE_LOADDEVICE
-}} = Messages
-
 export interface Event {
   message_type?: string
   message_enum: number
