d643781e7bf6b3732981a30e6c7fdfef2386f638
keepkeyjon <jon@keepkey.com>
2019-01-11 09:21:23 -0700

    Remove the message blacklist added in #1
    It breaks PassphraseAck's, and probably EntropyAck's too.  Client's *should* do
    this themselves before logging, but we can't easily do it for them.  Leave
    events alone for now.

--- src/event.ts
+++ src/event.ts
@@ -7,12 +7,6 @@ const { MessageType: {
   MESSAGETYPE_LOADDEVICE
 }} = Messages
 
-export const MSG_CONTENT_BLACKLIST = [
-  MESSAGETYPE_PASSPHRASEACK,
-  MESSAGETYPE_ENTROPYACK,
-  MESSAGETYPE_LOADDEVICE
-]
-
 export interface Event {
   message_type?: string
   message_enum: number
@@ -23,7 +17,6 @@ export interface Event {
 }
 
 export function makeEvent (e: Event): Event {
-  if (MSG_CONTENT_BLACKLIST.includes(e.message_enum)) e.message = {}
   return {
     message_type: messageNameRegistry[e.message_enum],
     date: Date.now(),
