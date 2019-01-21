dfc50fa2c620da0a0bfa433ca5bbb7663353d329
spiceboi <smc@shapeshift.io>
2019-01-20 20:10:17 -0700

    Add test for short but valid input

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -59,6 +59,9 @@ describe('utils', () => {
     test('formats return value correctly', () => {
       expect(utils.bip32ToAddressNList(`m/44'/60'/0'/0/0`)).toEqual([ -2147483604, -2147483588, -2147483648, 0, 0 ])
     })
+    test('will always return an array if the path looks valid', () => {
+      expect(utils.bip32ToAddressNList(`m/`)).toEqual([])
+    })
     test('throws when given an incorrect bip32 path', () => {
       expect(() => utils.bip32ToAddressNList(`/44'/60'/0'/0/0`)).toThrow()
     })
