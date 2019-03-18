2147ab9076026e315827570ffe1663316ac61681
keepkeyjon <jon@keepkey.com>
2019-03-18 17:43:43 -0600

    WIP: preferred account paths

--- src/hdwallet.ts
+++ src/hdwallet.ts
@@ -5,6 +5,15 @@ export abstract class WalletSupport {
   public abstract async bitcoinIsSupported (): Promise<boolean>
 }
 
+export interface EthereumGetAccountPaths {
+  coin: string,
+  accountIdx: number
+}
+
+export interface EthereumAccountPath {
+  addressNList: Array<number>
+}
+
 export interface EthereumGetAddress {
   addressNList: Array<number>,
   showDisplay?: boolean
@@ -23,6 +32,8 @@ export abstract class EthereumWallet {
   }
   public abstract async ethereumGetAddress (msg: EthereumGetAddress);
   public abstract async ethereumSignTx (msg: EthereumSignTx);
+
+  public abstract ethereumGetAccountPaths (msg: EthereumGetAccountPaths): Array<EthereumAccountPaths>;
 }
 
 export interface BitcoinGetAddress {
@@ -38,6 +49,25 @@ export interface BitcoinSignedTx {
   // ...
 }
 
+export enum InputScriptType {
+  IST_SpendAddress,
+  IST_SpendMultisig,
+  IST_External,
+  IST_SpendWitness,
+  IST_SpendP2SHWitness,
+}
+
+export interface BitcoinGetAccountPaths {
+  coin: string,
+  accountIdx: number,
+  scriptType?: InputScriptType
+}
+
+export interface BitcoinAccountPath {
+  scriptType: InputScriptType
+  addressNList: Array<number>
+}
+
 export abstract class BitcoinWallet implements WalletSupport {
   public static isSupported(wallet: WalletSupport): Promise<boolean> {
     return wallet.bitcoinIsSupported()
@@ -47,32 +77,41 @@ export abstract class BitcoinWallet implements WalletSupport {
   }
   public abstract async bitcoinGetAddress (msg: BitcoinGetAddress): Promise<BitcoinAddress>
   public abstract async bitcoinSignTx (msg: BitcoinSignTx): Promise<BitcoinSignedTx>
-}
 
-export enum InputScriptType {
-  IST_SpendAddress,
-  IST_SpendMultisig,
-  IST_External,
-  IST_SpendWitness,
-  IST_SpendP2SHWitness,
+  // Returns a list of bip32 paths for a given account index in preferred order
+  // from most to least preferred.
+  //
+  // For forked coins, eg. BSV, this would return:
+  //   p2pkh m/44'/236'/a'
+  //   p2pkh m/44'/230'/a'
+  //   p2pkh m/44'/0'/a'
+  //
+  // For BTC it might return:
+  //   p2sh-p2pkh m/49'/0'/a'
+  //   p2pkh      m/44'/0'/a'
+  //   p2sh-p2wsh m/44'/0'/a'
+  public abstract bitcoinGetAccountPaths (msg: BitcoinGetAccountPaths): Array<BitcoinAccountPath>
+
+  // Does the device support spending from the combined accounts?
+  public abstract bitcoinIsSameAccount (msg: Array<BitcoinAccountPath>): boolean
 }
 
 export interface GetPublicKey {
   addressNList: Array<number>,
-  ecdsaCurveName?: String,
+  ecdsaCurveName?: string,
   showDisplay?: boolean,
   scriptType?: InputScriptType
 }
 
 export interface PublicKey {
   node: any, // FIXME
-  xpub: String
+  xpub: string
 }
 
 export abstract class HDWallet implements WalletSupport {
-  public abstract async getVendor (): Promise<String>
+  public abstract async getVendor (): Promise<string>
 
-  public abstract async getModel (): Promise<String>
+  public abstract async getModel (): Promise<string>
 
   public abstract async getPublicKey (msg: GetPublicKey);
 
@@ -91,11 +130,11 @@ export abstract class HDWallet implements WalletSupport {
 class KeepKeyHDWallet implements HDWallet, EthereumWallet, BitcoinWallet {
   private impl: KeepKey
 
-  public abstract async getVendor (): Promise<String> {
+  public abstract async getVendor (): Promise<string> {
     return "keepkey.com"
   }
 
-  public abstract async getModel (): Promise<String> {
+  public abstract async getModel (): Promise<string> {
     return "KeepKey"
   }
 
@@ -130,12 +169,28 @@ class KeepKeyHDWallet implements HDWallet, EthereumWallet, BitcoinWallet {
 
   }
 
+  public bitcoinGetAccountPaths (msg: BitcoinGetAccountPaths): Array<BitcoinAccountPath> {
+    const slip44 = 0 // FIXME, assumes BTC
+    const ret: Array<BitcoinAccountPath> = []
+    ret.append({ addressNList: [ 0x80000000 + 44, 0x80000000 + slip44, 0x80000000 + msg.accountIdx ], scriptType: IST_SpendAddress })
+    ret.append({ addressNList: [ 0x80000000 + 49, 0x80000000 + slip44, 0x80000000 + msg.accountIdx ], scriptType: IST_SpendP2SHWitness })
+    ret.append({ addressNList: [ 0x80000000 + 84, 0x80000000 + slip44, 0x80000000 + msg.accountIdx ], scriptType: IST_SpendWitness })
+    return ret
+  }
+
   public async ethereumGetAddress (msg: EthereumGetAddress) {
 
   }
 
   public async ethereumSignTx (msg: EthereumSignTx) {
 
+  }
+
+  public ethereumGetAccountPaths (msg: EthereumAccountPaths): Array<EthereumAccountPath> {
+    const slip44 = 60 // FIXME, assumes ETH
+    const ret: Array<EthereumAccountPath> = []
+    ret.append({ addressNList: [ 0x80000000 + 44, 0x80000000 + slip44, 0x80000000 + msg.accountIdx, 0, 0 ] })
+    return ret
   }
 
   public async clearSession (): Promise<void> {
